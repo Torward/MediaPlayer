52ab0f58be99c952cb1308c2f8292481595caa0c
Torward <lomovfg@gmail.com>
2021-08-27 20:37:20 +1000

    MP base

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,17 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 import java.io.File;
 import java.net.URL;
@@ -24,40 +30,133 @@ public class Controller implements Initializable {
     private TimerTask task;
     private boolean running;
 
+    @FXML
     private Label songLabel;
+    @FXML
     private ProgressBar songProgressBar;
+    @FXML
     private Button playBTN;
+    @FXML
     private Button pauseBTN;
+    @FXML
     private Button rewBTN;
+    @FXML
     private Button ffwBTN;
+    @FXML
     private Button resetBTN;
+    @FXML
     private Slider volumeSlider;
 
-    public void playMedia(ActionEvent actionEvent) {
+    private Media media;
+    private MediaPlayer mediaPlayer;
+
+    public void playMedia() {
+        beginTimer();
+        mediaPlayer.play();
     }
 
-    public void pauseMedia(ActionEvent actionEvent) {
+    public void pauseMedia() {
+        mediaPlayer.pause();
     }
 
-    public void rewriteMedia(ActionEvent actionEvent) {
+    public void rewriteMedia() {
+        if (songNumber > 0) {
+            songNumber--;
+            mediaPlayer.stop();
+            if (running) {
+                cancelTimer();
+            }
+            media = new Media(songs.get(songNumber).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+            songLabel.setText(songs.get(songNumber).getName());
+            playMedia();
+        } else {
+            songNumber = songs.size() - 1;
+            mediaPlayer.stop();
+            if (running) {
+                cancelTimer();
+            }
+            media = new Media(songs.get(songNumber).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+            songLabel.setText(songs.get(songNumber).getName());
+            playMedia();
+        }
     }
 
     public void forwardMedia(ActionEvent actionEvent) {
+        if (songNumber < songs.size() - 1) {
+            songNumber++;
+            mediaPlayer.stop();
+            if (running){
+                cancelTimer();
+            }
+            media = new Media(songs.get(songNumber).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+            songLabel.setText(songs.get(songNumber).getName());
+            playMedia();
+        } else {
+            songNumber = 0;
+            mediaPlayer.stop();
+            if (running){
+                cancelTimer();
+            }
+            media = new Media(songs.get(songNumber).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+            songLabel.setText(songs.get(songNumber).getName());
+            playMedia();
+        }
     }
 
-    public void resetMedia(ActionEvent actionEvent) {
+    public void resetMedia() {
+        songProgressBar.setProgress(0);
+        mediaPlayer.seek(Duration.seconds(0.0));
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-            songs = new ArrayList<File>();
-            dir = new File();
+        songs = new ArrayList<File>();
+        dir = new File("music");
+        files = dir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                songs.add(file);
+                System.out.println(file);
+            }
+        }
+        media = new Media(songs.get(songNumber).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        songLabel.setText(songs.get(songNumber).getName());
+        volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
+            }
+        });
+        songProgressBar.setStyle("-fx-background: #000000;");
+        songProgressBar.setStyle("-fx-accent: #FF0000;");
+
+
     }
 
-    public void beginTimer(){
-
+    public void beginTimer() {
+        timer = new Timer();
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                running = true;
+                double current = mediaPlayer.getCurrentTime().toSeconds();
+                double end = media.getDuration().toSeconds();
+                songProgressBar.setProgress(current / end);
+                if (current / end == 1) {
+                    cancelTimer();
+                }
+            }
+        };
+        timer.scheduleAtFixedRate(task, 0, 1000);
     }
-    public void cancelTimer(){
 
+    public void cancelTimer() {
+        running = false;
+        timer.cancel();
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,13 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
@@ -14,6 +17,14 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 172));
         primaryStage.show();
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 
 
